fa259a58194ea0ae46c069a7c9d8439e4d22d050
Anton Reva <boss.kvanta@gmail.com>
2022-09-11 17:38:22 +0300

    Исправил ошибку в 6 задании

--- js/task-06.js
+++ js/task-06.js
@@ -2,14 +2,8 @@ const input = document.querySelector("#validation-input");
 input.addEventListener("blur", inputBlur);
 
 function inputBlur(event) {
+  input.classList.remove("invalid");
   event.currentTarget.value.length > input.dataset.length
     ? input.classList.add("valid")
     : input.classList.add("invalid");
-
-  //   if (event.currentTarget.value.length > input.dataset.length) {
-  //     input.classList.add("valid");
-  //     console.log(event.currentTarget.value);
-  //   } else {
-  //     input.classList.add("invalid");
-  //   }
 }
